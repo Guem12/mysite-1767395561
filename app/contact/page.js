007b338eca18949013cb45220c13c14917ca1ab6
Guem12 <beidydia99@gmail.com>
2026-01-02 23:12:50 +0000

    Add app/contact/page.js

---
+++ app/contact/page.js
@@ -0,0 +1,14 @@
+'use client'
+
+import { useState } from 'react'
+
+export default function ContactPage() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  })
+
+  const handleChange = (e) => {
+    setForm
